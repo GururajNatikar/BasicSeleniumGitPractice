396e89c77f8e3e0475297ed01b434c83527bf345
hp <hp@DESKTOP-GURU-NATIKAR>
2025-03-19 02:13:10 +0530

    Demo2 branch committed

--- src/test/java/Practice/DemoclassTest.java
+++ src/test/java/Practice/DemoclassTest.java
@@ -5,7 +5,7 @@ public class DemoclassTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo Test executed");
-		System.out.println("Demo brach pushed");
+		System.out.println("Demo2 brach pushed");
 	}
 
 }
